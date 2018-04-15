adf0a5b47a72983095533356049ecd3171285438
valdiss <valentin.dissais@gmail.com>
2018-04-15 13:11:41 +0200

    before first assignment

--- src/App.js
+++ src/App.js
@@ -1,16 +1,64 @@
 import React, { Component } from "react";
 import "./App.css";
-import Person from "./Person/Person";
+import Button from "material-ui/Button";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Max", age: 28 },
+      { name: "Manu", age: 29 },
+      { name: "Stephanie", age: 25 }
+    ],
+    otherState: "some other value"
+  };
+
+  switchNameHandler = newName => {
+    // console.log("was clicked!");
+    this.setState({
+      persons: [
+        { name: newName, age: 28 },
+        { name: "Manu", age: 29 },
+        { name: "Stephanie", age: 21 }
+      ]
+    });
+  };
+
+  nameChangedHandler = event => {
+    this.setState({
+      persons: [
+        { name: "Max", age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: "Stephanie", age: 21 }
+      ]
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I 'm a React App</h1>
         <p>This is really working!</p>
-        <button>Switch Name</button>
-        <Person name="Val" age="29" />
-        <Person name="Georges" age="58">
+        <Button
+          variant="raised"
+          color="primary"
+          onClick={() => this.switchNameHandler("Test")}
+        >
+          Switch Name
+        </Button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          changed={this.nameChangedHandler}
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
+        <Person
+          click={this.switchNameHandler.bind(this, "Valentin")}
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        >
           My Hobbies: Music
         </Person>
       </div>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,16 @@
 import React from "react";
+import Input from "material-ui/TextField";
+
+import "./Person.css";
 
 const person = props => {
   return (
-    <div>
-      <p>
+    <div className="Person">
+      <p onClick={props.click}>
         I'm {props.name} and I am {props.age} years old!
       </p>
       <p>{props.children}</p>
+      <Input type="text" onChange={props.changed} value={props.name} />
     </div>
   );
 };
